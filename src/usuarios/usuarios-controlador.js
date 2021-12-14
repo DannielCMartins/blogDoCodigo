a7d42b3ad26588497f60833cab20beb5b4bb2fe3
DannielCMartins <53942956+DannielCMartins@users.noreply.github.com>
2021-12-14 14:23:52 -0300

    melhorias

--- src/usuarios/usuarios-controlador.js
+++ src/usuarios/usuarios-controlador.js
@@ -2,7 +2,11 @@ const Usuario = require('./usuarios-modelo');
 const { InvalidArgumentError } = require('../erros');
 
 const jwt = require('jsonwebtoken');
-const blocklist = require('../../redis/manipula-blocklist');
+const blocklist = require('../../redis/blocklist-access-token');
+const allowlist = require('../../redis/allowlist-refresh-token');
+
+const crypto = require('crypto');
+const moment = require('moment');
 
 function criaTokenJWT(usuario) {
   const payload = {
@@ -12,6 +16,15 @@ function criaTokenJWT(usuario) {
   const token = jwt.sign(payload, process.env.CHAVE_JWT, { expiresIn: '15m' });
   return token;
 }
+
+async function criaTokenOpaco(usuario) {
+  const tokenOpaco = crypto.randomBytes(24).toString('hex');
+  const dataExpiracao = moment().add(5, 'd').unix();
+  await allowlist.adiciona(tokenOpaco, usuario.id, dataExpiracao);
+
+  return tokenOpaco;
+}  
+
 
 module.exports = {
   async adiciona(req, res) {
@@ -36,9 +49,10 @@ module.exports = {
 
   async login(req, res) {
     try {
-      const token = criaTokenJWT(req.user);
-      res.set('Authorization', token);
-      res.status(204).json();
+      const AccessToken = criaTokenJWT(req.user);
+      const refreshToken = await criaTokenOpaco(req.user)
+      res.set('Authorization', AccessToken);
+      res.status(200).json({ refreshToken });
     } catch (erro) {
       res.status(500).json({ erro: erro.message });
     }
